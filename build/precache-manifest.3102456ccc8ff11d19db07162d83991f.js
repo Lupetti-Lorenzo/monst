5b415277bb93b4b30230f924d7cd1c53c7824f9a
Lorenzo <Lupetti>
2020-04-08 13:55:44 +0200

    files for github pages

---
+++ build/precache-manifest.3102456ccc8ff11d19db07162d83991f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00c306f46689aae0e6e26ab5757a545",
+    "url": "/monst/index.html"
+  },
+  {
+    "revision": "7d0d77440172e8e818d2",
+    "url": "/monst/static/css/main.1422ed7e.chunk.css"
+  },
+  {
+    "revision": "750ee232caa1c9bcce9d",
+    "url": "/monst/static/js/2.29b92bbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monst/static/js/2.29b92bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0d77440172e8e818d2",
+    "url": "/monst/static/js/main.efd8bcb9.chunk.js"
+  },
+  {
+    "revision": "72cc370dcf8465a2e07a",
+    "url": "/monst/static/js/runtime-main.90c81566.js"
+  }
+]);
